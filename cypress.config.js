3b534e58105884d2e9d300f3c58f229a23075ceb
Juan David <juan.munozv@perficient.com>
2022-06-30 11:32:24 -0500

    taking out video and screenshot from config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    video: false,
-    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  video: false,
+  screenshotOnRunFailure: false
 });
