eebb207d0336c9539ac1662a06f43f7fb7931a32
Juan David <juan.munozv@perficient.com>
2022-06-30 11:04:21 -0500

    continuos integration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    "video": false,
+    "screenshotOnRunFailure": false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
