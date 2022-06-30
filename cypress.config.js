5bb9927d79f02245e044f38183a2ba1db0f8ce42
Juan David <juan.munozv@perficient.com>
2022-06-30 11:20:45 -0500

    fixing the data into cypress.config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    "video": false,
-    "screenshotOnRunFailure": false,
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
