6f76ddff2eb638418a68bdc25dee444b846e1985
Juan David <juan.munozv@perficient.com>
2022-07-06 16:02:56 -0500

    improving reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,24 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
+    defaultCommandTimeout: 60000,
+    responseTimeout: 60000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    retries: 2,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      reporterEnabled: "mochawesome",
+      mochawesomeReporterOptions: {
+        reportDir: "cypress/reports/mocha",
+        quite: true,
+        overwrite: false,
+        html: false,
+        json: true,
+      },
+    },
   },
-  video: false,
-  screenshotOnRunFailure: false,
-  defaultCommandTimeout: 20000,
-  responseTimeout: 20000
 });
