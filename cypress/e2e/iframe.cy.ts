57ce621d1c918ec98b48b6c325eb502772e3cd03
Juan David <juan.munozv@perficient.com>
2022-07-22 17:22:02 -0500

    fixing the way of how to get the iframe titles

--- cypress/e2e/iframe.cy.ts
+++ cypress/e2e/iframe.cy.ts
@@ -3,13 +3,19 @@ import {IframePage} from "../page/index";
 describe("Working with Iframe", () => {
   let iframePage: IframePage;
   const htmlText = "HTML Tutorial";
+  const cssText = "CSS Tutorial";
   beforeEach(() => {
     iframePage = new IframePage();
+    iframePage.visit();
   });
 
-  it("Iframe page", () => {
-    iframePage.visit();
-    iframePage.getFrameTitle(htmlText);
+  it("Iframe HTML page", () => {
+    iframePage.getFrameTitleHTML().should("have.text", htmlText);
+  });
+
+  it.only("Iframe CSS page", () => {
     iframePage.goToCssPageInFrame();
+    // cy.wait(1000);
+    iframePage.getFrameTitleCSS().should("have.text", cssText);
   });
 });
--- cypress/page/iframe.page.ts
+++ cypress/page/iframe.page.ts
@@ -3,24 +3,30 @@ class IframePage {
     private iFrameSite: string;
     private iFrameSelector: string;
     private cssSelector: string;
-
+    private selectorH1: string;
     
     constructor() {
       this.iFrameSite = "https://www.w3schools.com/html/html_iframe.asp";
       this.iFrameSelector = "[title='W3Schools HTML Tutorial']";
       this.cssSelector = "[title='CSS Tutorial']"
+      this.selectorH1 = "//h1"
     }
   
     public visit(): void {
       cy.visit(this.iFrameSite);
     }
   
-    public getFrameTitle(htmlText: string): void {
-      cy.iframe(this.iFrameSelector).find('h1').should("have.text", htmlText);
+    public getFrameTitleHTML() {
+      return cy.iframe(this.iFrameSelector).find('h1');
     }
   
     public goToCssPageInFrame(): void{
       cy.iframe(this.iFrameSelector).find(this.cssSelector).click();  
+    }
+
+    public getFrameTitleCSS() {
+     cy.wait(1000);
+     return cy.iframe(this.iFrameSelector).find('h1');
     }
   
   }
